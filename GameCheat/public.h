02703063a6ae7a92690dba9dffb95c8faef45995
HackrJi <60073425+iHackerJi@users.noreply.github.com>
2022-02-24 18:04:00 +0800

    添加项目文件。

---
+++ GameCheat/public.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <windows.h>
+#include <iostream>
+#include <string.h>
+
+#include "tools.h"
+
---
+++ GameCheat/tools.h
@@ -0,0 +1,6 @@
+#pragma once
+namespace tools
+{
+    std::string utf8_to_string(const std::string& str);
+    std::string string_to_utf8(const std::string& str);
+}
